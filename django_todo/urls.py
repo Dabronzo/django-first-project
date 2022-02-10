0bea2806fe684d217ce37ffb7d9ed3e0d8fd1a7a
Dabronzo <ayrtonterceiro@gmail.com>
2022-02-10 21:23:37 +0000

    created the model database for items

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo.views import say_hello
+from todo.views import get_todo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', say_hello, name='hello')
+    path('', get_todo, name='get_todo')
 ]
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Itens(models.Model):
+    """ Model class to create the itens for the database
+    Notice that null=false means that the name need to have a value
+    and can not be an empty value"""
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
+
+    # here we gonna rewrite on function in the model documentation to display a better name of the item
+    # in the admin page
+
+    def __str__(self):
+        return f"{self.name}"
